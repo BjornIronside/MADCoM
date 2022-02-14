ddc2e94f64d708e4edd9a233a05780e11d5424e4
BjornIronside <diogo.oliveira898@gmail.com>
2022-02-14 16:02:22 +0000

    Even more comparison results
    30 minutes
    5 runs
    Instances:
    egl-g2-E
    Hefei-10
    F1_g-6
    Beijing-3
    Beijing-5

--- Program/automaton.bat
+++ Program/automaton.bat
@@ -4,6 +4,21 @@ ECHO OFF
 ECHO gencarp
 ECHO gencarp >> log.txt
 
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5 >> log.txt
+gencarp "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5 >> log.txt
+gencarp "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5 >> log.txt
+gencarp "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5 >> log.txt
+gencarp "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5 >> log.txt
+gencarp "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
 ECHO %DATE% %TIME% - F1_g-6 Run 1/5
 ECHO %DATE% %TIME% - F1_g-6 Run 1/5 >> log.txt
 gencarp "../Instances/CARP/F1_g-6.txt" -type 30 -t 1800
@@ -34,6 +49,21 @@ gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 1800
 ECHO %DATE% %TIME% - Beijing-3 Run 5/5
 ECHO %DATE% %TIME% - Beijing-3 Run 5/5 >> log.txt
 gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 1/5
+ECHO %DATE% %TIME% - Hefei-10 Run 1/5 >> log.txt
+gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 2/5
+ECHO %DATE% %TIME% - Hefei-10 Run 2/5 >> log.txt
+gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 3/5
+ECHO %DATE% %TIME% - Hefei-10 Run 3/5 >> log.txt
+gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 4/5
+ECHO %DATE% %TIME% - Hefei-10 Run 4/5 >> log.txt
+gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 5/5
+ECHO %DATE% %TIME% - Hefei-10 Run 5/5 >> log.txt
+gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
 ECHO %DATE% %TIME% - Beijing-5 Run 1/5
 ECHO %DATE% %TIME% - Beijing-5 Run 1/5 >> log.txt
 gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 1800
@@ -55,6 +85,21 @@ gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 1800
 ECHO madcom
 ECHO madcom >> log.txt
 
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 1800
 ECHO %DATE% %TIME% - F1_g-6 Run 1/5
 ECHO %DATE% %TIME% - F1_g-6 Run 1/5 >> log.txt
 madcom "../Instances/CARP/F1_g-6.txt" -type 30 -t 1800
@@ -85,6 +130,21 @@ madcom "../Instances/CARP/Beijing-3.txt" -type 30 -t 1800
 ECHO %DATE% %TIME% - Beijing-3 Run 5/5
 ECHO %DATE% %TIME% - Beijing-3 Run 5/5 >> log.txt
 madcom "../Instances/CARP/Beijing-3.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 1/5
+ECHO %DATE% %TIME% - Hefei-10 Run 1/5 >> log.txt
+madcom "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 2/5
+ECHO %DATE% %TIME% - Hefei-10 Run 2/5 >> log.txt
+madcom "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 3/5
+ECHO %DATE% %TIME% - Hefei-10 Run 3/5 >> log.txt
+madcom "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 4/5
+ECHO %DATE% %TIME% - Hefei-10 Run 4/5 >> log.txt
+madcom "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
+ECHO %DATE% %TIME% - Hefei-10 Run 5/5
+ECHO %DATE% %TIME% - Hefei-10 Run 5/5 >> log.txt
+madcom "../Instances/CARP/Hefei-10.txt" -type 30 -t 1800
 ECHO %DATE% %TIME% - Beijing-5 Run 1/5
 ECHO %DATE% %TIME% - Beijing-5 Run 1/5 >> log.txt
 madcom "../Instances/CARP/Beijing-5.txt" -type 30 -t 1800
