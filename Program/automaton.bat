0d93c8353128843aa1b315a6012aca499be8f70b
BjornIronside <diogo.oliveira898@gmail.com>
2022-03-14 15:55:08 +0000

    MADCoM with HD and RCO Results
    MADCoM with HD and RCO
    10 runs
    30 minutes
    Instances:
    egl-g2-E
    Hefei-10
    F1_g-6
    Beijing-3
    Beijing-5

--- Program/automaton.bat
+++ Program/automaton.bat
@@ -1,56 +1,5 @@
 ECHO OFF
 
-::gencarp
-ECHO gencarp
-ECHO gencarp >> log.txt
-
-ECHO %DATE% %TIME% - Beijing-3 Run 1/5
-ECHO %DATE% %TIME% - Beijing-3 Run 1/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-3 Run 2/5
-ECHO %DATE% %TIME% - Beijing-3 Run 2/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-3 Run 3/5
-ECHO %DATE% %TIME% - Beijing-3 Run 3/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-3 Run 4/5
-ECHO %DATE% %TIME% - Beijing-3 Run 4/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-3 Run 5/5
-ECHO %DATE% %TIME% - Beijing-3 Run 5/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Hefei-10 Run 1/5
-ECHO %DATE% %TIME% - Hefei-10 Run 1/5 >> log.txt
-gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Hefei-10 Run 2/5
-ECHO %DATE% %TIME% - Hefei-10 Run 2/5 >> log.txt
-gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Hefei-10 Run 3/5
-ECHO %DATE% %TIME% - Hefei-10 Run 3/5 >> log.txt
-gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Hefei-10 Run 4/5
-ECHO %DATE% %TIME% - Hefei-10 Run 4/5 >> log.txt
-gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Hefei-10 Run 5/5
-ECHO %DATE% %TIME% - Hefei-10 Run 5/5 >> log.txt
-gencarp "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-5 Run 1/5
-ECHO %DATE% %TIME% - Beijing-5 Run 1/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-5 Run 2/5
-ECHO %DATE% %TIME% - Beijing-5 Run 2/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-5 Run 3/5
-ECHO %DATE% %TIME% - Beijing-5 Run 3/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-5 Run 4/5
-ECHO %DATE% %TIME% - Beijing-5 Run 4/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
-ECHO %DATE% %TIME% - Beijing-5 Run 5/5
-ECHO %DATE% %TIME% - Beijing-5 Run 5/5 >> log.txt
-gencarp "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
-
-
 ::madcom
 ECHO madcom
 ECHO madcom >> log.txt
@@ -100,5 +49,116 @@ madcom "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
 ECHO %DATE% %TIME% - Beijing-5 Run 5/5
 ECHO %DATE% %TIME% - Beijing-5 Run 5/5 >> log.txt
 madcom "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5 >> log.txt
+madcom "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 1/5
+ECHO %DATE% %TIME% - F1_g-6 Run 1/5 >> log.txt
+madcom "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 2/5
+ECHO %DATE% %TIME% - F1_g-6 Run 2/5 >> log.txt
+madcom "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 3/5
+ECHO %DATE% %TIME% - F1_g-6 Run 3/5 >> log.txt
+madcom "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 4/5
+ECHO %DATE% %TIME% - F1_g-6 Run 4/5 >> log.txt
+madcom "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 5/5
+ECHO %DATE% %TIME% - F1_g-6 Run 5/5 >> log.txt
+madcom "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+
+
+::madcom_hd
+ECHO madcom_hd
+ECHO madcom_hd >> log.txt
+
+ECHO %DATE% %TIME% - Beijing-3 Run 1/5
+ECHO %DATE% %TIME% - Beijing-3 Run 1/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-3 Run 2/5
+ECHO %DATE% %TIME% - Beijing-3 Run 2/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-3 Run 3/5
+ECHO %DATE% %TIME% - Beijing-3 Run 3/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-3 Run 4/5
+ECHO %DATE% %TIME% - Beijing-3 Run 4/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-3 Run 5/5
+ECHO %DATE% %TIME% - Beijing-3 Run 5/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-3.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Hefei-10 Run 1/5
+ECHO %DATE% %TIME% - Hefei-10 Run 1/5 >> log.txt
+madcom_hd "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Hefei-10 Run 2/5
+ECHO %DATE% %TIME% - Hefei-10 Run 2/5 >> log.txt
+madcom_hd "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Hefei-10 Run 3/5
+ECHO %DATE% %TIME% - Hefei-10 Run 3/5 >> log.txt
+madcom_hd "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Hefei-10 Run 4/5
+ECHO %DATE% %TIME% - Hefei-10 Run 4/5 >> log.txt
+madcom_hd "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Hefei-10 Run 5/5
+ECHO %DATE% %TIME% - Hefei-10 Run 5/5 >> log.txt
+madcom_hd "../Instances/CARP/Hefei-10.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-5 Run 1/5
+ECHO %DATE% %TIME% - Beijing-5 Run 1/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-5 Run 2/5
+ECHO %DATE% %TIME% - Beijing-5 Run 2/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-5 Run 3/5
+ECHO %DATE% %TIME% - Beijing-5 Run 3/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-5 Run 4/5
+ECHO %DATE% %TIME% - Beijing-5 Run 4/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - Beijing-5 Run 5/5
+ECHO %DATE% %TIME% - Beijing-5 Run 5/5 >> log.txt
+madcom_hd "../Instances/CARP/Beijing-5.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5
+ECHO %DATE% %TIME% - egl-g2-E Run 1/5 >> log.txt
+madcom_hd "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5
+ECHO %DATE% %TIME% - egl-g2-E Run 2/5 >> log.txt
+madcom_hd "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5
+ECHO %DATE% %TIME% - egl-g2-E Run 3/5 >> log.txt
+madcom_hd "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5
+ECHO %DATE% %TIME% - egl-g2-E Run 4/5 >> log.txt
+madcom_hd "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5
+ECHO %DATE% %TIME% - egl-g2-E Run 5/5 >> log.txt
+madcom_hd "../Instances/CARP/egl-g2-E.dat" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 1/5
+ECHO %DATE% %TIME% - F1_g-6 Run 1/5 >> log.txt
+madcom_hd "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 2/5
+ECHO %DATE% %TIME% - F1_g-6 Run 2/5 >> log.txt
+madcom_hd "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 3/5
+ECHO %DATE% %TIME% - F1_g-6 Run 3/5 >> log.txt
+madcom_hd "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 4/5
+ECHO %DATE% %TIME% - F1_g-6 Run 4/5 >> log.txt
+madcom_hd "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
+ECHO %DATE% %TIME% - F1_g-6 Run 5/5
+ECHO %DATE% %TIME% - F1_g-6 Run 5/5 >> log.txt
+madcom_hd "../Instances/CARP/F1_g-6.txt" -type 30 -t 3600
 
 
